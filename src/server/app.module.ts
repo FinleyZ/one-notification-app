dd8575d76d985b0d6812f4f75aad3fe63e992348
Finley <hongfeiz@mun.ca>
2023-10-25 18:00:32 -0300

    setup monorepo for next.js

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import Next from 'next';
+import { RenderModule } from 'nest-next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    RenderModule.forRootAsync(Next({
+          dev: process.env.NODE_ENV !== 'production',
+          dir: resolve(__dirname, '..'),
+        }),
+        { passthrough404: true, viewsDir: null },),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
