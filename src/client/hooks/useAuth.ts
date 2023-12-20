45254cb75c41bbe64569e088d468db187dc05f29
Finley <hongfeiz@mun.ca>
2023-12-20 13:50:42 -0400

    remove front-end starts over

--- src/client/hooks/useAuth.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from 'src/client/contexts/Auth0Context';
-
-export const useAuth = () => useContext(AuthContext);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,7 @@
-import { Box, Card, Container, styled } from '@mui/material';
-import type { ReactElement } from 'react';
-import BaseLayout from 'src/client/layouts/BaseLayout';
+import { FC } from 'react';
 
-import Head from 'next/head';
-import Logo from 'src/client/components/LogoSign';
-import Hero from 'src/client/content/overview/Hero';
-import Footer from 'src/client/components/Footer';
-
-const HeaderWrapper = styled(Card)(
-  ({ theme }) => `
-  width: 100%;
-  display: flex;
-  align-items: center;
-  height: ${theme.spacing(10)};
-  margin-bottom: ${theme.spacing(10)};
-`,
-);
-
-const OverviewWrapper = styled(Box)(
-  ({ theme }) => `
-    overflow: auto;
-    background: ${theme.palette.common.white};
-    flex: 1;
-    overflow-x: hidden;
-`,
-);
-
-function Overview() {
-  return (
-    <OverviewWrapper>
-      <Head>
-        <title>One Notefictation Admin Dashboard</title>
-      </Head>
-      <HeaderWrapper>
-        <Container maxWidth="lg">
-          <Box display="flex" alignItems="center">
-            <Logo />
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              flex={1}
-            >
-              <Box />
-              <Box>
-                {/* <Button
-                  component={Link}
-                  href="/dashboards/reports"
-                  variant="contained"
-                  sx={{ ml: 2 }}
-                >
-                  Live Preview
-                </Button> */}
-              </Box>
-            </Box>
-          </Box>
-        </Container>
-      </HeaderWrapper>
-      <Hero />
-      <Footer />
-    </OverviewWrapper>
-  );
-}
-
-export default Overview;
-
-Overview.getLayout = function getLayout(page: ReactElement) {
-  return <BaseLayout>{page}</BaseLayout>;
+const Home: FC = () => {
+    return <h1>Home</h1>;
 };
+
+export default Home;
