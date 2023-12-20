51939d56019f3ab4933579f12192a8d478a31d26
Finley <hongfeiz@mun.ca>
2023-12-20 14:00:06 -0400

    front-end restart

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { AppProps } from 'next/app';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 };
 
 export default App;
